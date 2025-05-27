002ae9437c9a0c034f3004f0ff416d4b43671bd8
Martin Kulicek <mkulicek@trask.cz>
2025-05-27 14:02:16 +0200

    Pridal se minus kokote

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-# Nejakej fofor demo priklad pro CICD, nehledej v tom logiku
+# Nejakej fofor demo priklad pro CICD, nehledej v tom logiku nejakyho jadernyho reaktoru
 
 def add(a, b):
     return a + b
+
+
+def minus(a, b):
+    return a - b
--- test_main.py
+++ test_main.py
@@ -1,5 +1,9 @@
-from main import add
+#ez peez testiky jako ukazka asi staci
+from main import *
 
 
 def test_add():
     assert add(5, 8) == 13
+
+def test_minus():
+    assert minus(10,5) == 5
